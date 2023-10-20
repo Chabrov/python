851873deab2be0136eba66b333c8d207898f608c
Vchabrov <vchabrov@gmail.com>
2023-10-20 15:37:12 +0300

    Изменено описание репозитория

---
+++ test_go_back.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+class TestGoBack(unittest.TestCase):
+    def setUp(self):
+        self.wd = webdriver.Chrome(executable_path=r'')
+        self.wd.implicitly_wait(30)
+        self.base_url = "https://www.google.com/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def test_go_back(self):
+        wd = self.wd
+        #ERROR: Caught exception [unknown command []]
+        wd.find_element_by_xpath("//a[@id='profile.go_to_welcome']/p").click()
+        wd.get("https://my.katalon.com/welcome")
+    
+    def is_element_present(self, how, what):
+        try: self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.wd.switch_to_alert()
+        except NoAlertPresentException as e: return False
+        return True
+    
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.wd.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+    
+    def tearDown(self):
+        self.wd.quit()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
